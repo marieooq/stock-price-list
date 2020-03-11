6adecab492340777fe1d1ca75a9c2369abe8f49c
Marie Otaki <otakimarie@gmail.com>
2020-03-10 19:11:23 -0700

    set symbols to state

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,27 +2,43 @@ import React, { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import { any } from 'prop-types';
 
+interface RsponseElement {
+  symbol: string;
+  exchange: string;
+  name: string;
+  date: string;
+  type: string;
+  iexId: string;
+  region: string;
+  currency: string;
+  isEnabled: boolean;
+  figi: string;
+  cik: string;
+}
+
 const Test = () => {
-  const [data, setData] = useState<string>('');
+  const [symbol, setSymbol] = useState<string>('');
 
   useEffect((): void => {
-    axios
-      .get(
-        'https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_1f9b377e6b664b4684333c106015e21d'
-      )
-      .then((res: AxiosResponse) => {
-        const getData = res.data;
-        for (const item of getData) {
-          console.log(item);
-          //   setData(item);
-        }
-      })
-      .catch(error => {
+    const fetchAllSymbols = async () => {
+      try {
+        const res = await axios.get(
+          'https://cloud.iexapis.com/stable/ref-data/symbols?token=pk_1f9b377e6b664b4684333c106015e21d'
+        );
+        const items = res.data;
+        const symbols = items.map((val: RsponseElement) => {
+          return val.symbol;
+        });
+        setSymbol(symbols);
+      } catch (error) {
         console.log(error);
-      });
-  });
+      }
+    };
 
-  return <>{data}</>;
+    fetchAllSymbols();
+  }, []);
+
+  return <>{symbol}</>;
 };
 
 export default Test;
